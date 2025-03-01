6b0a51b6be8c655342f02d86c77816b6c5f06ac6
Andy Nguyen <iam.andynguwin@gmail.com>
2025-03-01 11:56:19 -0800

    Created backend folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api")
+      .then((response) => setMessage(response.data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <>
+      <div>
+        <h1>Frontend-Backend Connection</h1>
+        <p>{message}</p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
