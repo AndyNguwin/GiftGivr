a9937272c6c439f7dc83d51e22dd698fd123d2b1
Rachael Le <rachaelle@MacBookPro.lan>
2025-03-01 16:31:49 -0800

    homepage needs styling tho

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,18 @@
 // Home.tsx
 import React from 'react';
 import './Home.css';
-import { Button } from '@mui/material';
-import presents from '../assets/presents.png';
+import { Button } from '@mui/material';;
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <h1>Welcome to the Home Page</h1>
+      <h1>GiftGivr</h1>
       <p>Create a wishlist with all your favorite items for your friends and family to shop from.
          Connect with your loved ones to see what they want for the Holidays and keep track of their birthday!
         Never forget a birthday or gift idea again!
       </p>
       <Button className="home-button" variant='contained'>Join Today!</Button>
-      <img src={presents} alt="Presents" /> 
+      <img className="home-img" src="src/assets/pexels-pixabay-17796.jpg" alt="Presents"/> 
     </div>
   );
 }
