b54930d9076007577132e6dc6899c6a513cb39b8
Rachael Le <rachaelle@MacBookPro.lan>
2025-03-01 19:38:02 -0800

    edited the home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,16 +6,19 @@ import { Button } from '@mui/material';;
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <h1>GiftGivr</h1>
-      <p>Create a wishlist with all your favorite items for your friends and family to shop from.
-         Connect with your loved ones to see what they want for the Holidays and keep track of their birthday!
-        Never forget a birthday or gift idea again!
-      </p>
-      <Button 
-        className="home-button" 
-        variant='contained'
-        sx={{ backgroundColor: 'purple', '&:hover': { backgroundColor: 'darkpurple' } }} >
-        Join Today!</Button>
+      <div className="home-content">
+        <h1>GiftGivr</h1>
+        <p>Create a wishlist with all your favorite items for your friends and family to shop from.
+          Connect with your loved ones to see what they want for the Holidays and keep track of their birthday!
+          Never forget a birthday or gift idea again!
+        </p>
+        <Button 
+          className="home-button" 
+          variant='contained'
+          href="/createAccount"
+          sx={{ backgroundColor: 'purple', '&:hover': { backgroundColor: 'darkpurple' } }} >
+          Join Today!</Button>
+        </div>
       <img className="home-img" src="src/assets/pexels-pixabay-17796.jpg" alt="Presents"/> 
     </div>
   );
